a0b9ceeab76eac57c1373ab3915bacb5639e6f8a
Preston Lloyd <plloyd241@gmail.com>
2020-01-04 15:11:09 -0700

    Getting Started

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace GetNada
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            client.BaseAddress = new Uri("https://getnada.com/api/v1/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json")
+            );
+
+            string cmd = args[0];
+
+            Console.WriteLine("Hello World!");
+        }
+
+        static async Task GetDomains()
+        {}
+
+        static async Task GetNewEmail()
+        {}
+    }
+}
